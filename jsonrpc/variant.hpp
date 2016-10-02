3318da7b5a955ee81a5f68bb87bf53cf73d18742
Sergey Zubov <cybosser@gmail.com>
2016-10-02 18:50:52 +0300

    add wrapper for mapbox/variant

---
+++ jsonrpc/variant.hpp
@@ -0,0 +1,13 @@
+#ifndef JSONRPC_VARIANT_HPP
+#define JSONRPC_VARIANT_HPP
+
+#include <jsonrpc/thirdparty/mapbox/variant/variant.hpp>
+
+namespace jsonrpc
+{
+
+	using namespace mapbox::util;
+
+}
+
+#endif
